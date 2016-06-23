579d49b40d08367067eff0a7495ccd44c823eeae
arjunm13 <arjunmahen13@gmail.com>
2016-06-23 14:08:19 -0400

    created static serer

--- app.js
+++ app.js
@@ -1,43 +1,5 @@
-var inputLIst = [{
-    id: 'e',
-    parent_id: 'd',
-}, {
-    id: 'a',
-    parent_id: null,
-}, {
-    id: 'b',
-    parent_id: null,
-}, {
-    id: 'c',
-    parent_id: null,
-}, {
-    id: 'd',
-    parent_id: 'a',
-}, {
-    id: 'f',
-    parent_id: 'e',
-}, {
-    id: 'g',
-    parent_id: 'b',
-}, ];
-
-var map = {};
-var output = [];
-
-inputLIst.forEach(function(node) {
-    map[node.id] = { node: node, children: [] };
-
-    if (node.parent_id == null) {
-
-        output.push(map[node.id]);
-    }
-    Object.keys(map).forEach(function(key) {
-
-        if (map[key].node.parent_id) {
-            map[map[key].node.parent_id].node.children.push[map[key]];
-        }
-    });
-
-    console.log(output);
-
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
 });
